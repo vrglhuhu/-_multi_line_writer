5e9534259580aec43033c14da909bf15a9fd997f
vrglhuhu <cheanbvergel@gmail.com>
2023-04-22 22:42:01 +0800

    Creating txt file and Psuedocodes.

--- _multi_line_writer.py
+++ _multi_line_writer.py
@@ -1,3 +1,25 @@
 # Vergel, Chean Bernard Villanueva
 # Assignment No. 4
-# Question No. 2
+# Question No. 3
+
+
+# DEFINE write_to_file
+ 
+ # OPEN file 'mylife.txt' in write mode
+ 
+  # SET line equal to user input of a line of text
+
+  # WRITE line to file with newline character
+
+  # SET line_choice equal to user input of 'y' or 'n'
+
+  # WHILE line_choice is equal to 'y'
+  
+    # SET line equal to user input of a line of text
+   
+    # WRITE line to file with newline character
+  
+    # SET line_choice equal to user input of 'y' or 'n'
+   
+  # CLOSE file
+ 
