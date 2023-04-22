2e48608af95b02d191829f02c7e482ad3dcdebf7
vrglhuhu <cheanbvergel@gmail.com>
2023-04-22 22:53:59 +0800

    Creating def and opening the mylife.txt as file.

--- _multi_line_writer.py
+++ _multi_line_writer.py
@@ -16,21 +16,24 @@ print("\033[33mHi, I am Chean Bernard V. Vergel a first year college student at 
 print("")
 
 # DEFINE write_to_file
-
+def write_to_file():
  # OPEN file 'mylife.txt' in write mode
-
+ with open('mylife.txt', 'w') as file:
   # SET line equal to user input of a line of text
-
+ 
   # WRITE line to file with newline character
-
+  
   # SET line_choice equal to user input of 'y' or 'n'
-
+  
   # WHILE line_choice is equal to 'y'
-
+  
     # SET line equal to user input of a line of text
-
+    
     # WRITE line to file with newline character
-
+   
     # SET line_choice equal to user input of 'y' or 'n'
-
+    
   # CLOSE file
+ 
+  
+
