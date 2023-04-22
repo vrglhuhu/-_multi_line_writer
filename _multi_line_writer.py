9ece7cd9b297f0e58d528638b55273ac6923cee2
vrglhuhu <cheanbvergel@gmail.com>
2023-04-22 22:49:47 +0800

    Creating Header.

--- _multi_line_writer.py
+++ _multi_line_writer.py
@@ -2,11 +2,23 @@
 # Assignment No. 4
 # Question No. 3
 
+# Creating header 
+import pyfiglet
+
+print("")
+print("=" * 80)
+print("")
+welcome = pyfiglet.figlet_format("Welcome to my space".center(57, " "), font="digital")
+print(welcome)
+print("")
+print("=" * 80)
+print("\033[33mHi, I am Chean Bernard V. Vergel a first year college student at Polytechnic University of the Philippines.\033[0m")
+print("")
 
 # DEFINE write_to_file
- 
+
  # OPEN file 'mylife.txt' in write mode
- 
+
   # SET line equal to user input of a line of text
 
   # WRITE line to file with newline character
@@ -14,12 +26,11 @@
   # SET line_choice equal to user input of 'y' or 'n'
 
   # WHILE line_choice is equal to 'y'
-  
+
     # SET line equal to user input of a line of text
-   
+
     # WRITE line to file with newline character
-  
+
     # SET line_choice equal to user input of 'y' or 'n'
-   
+
   # CLOSE file
- 
