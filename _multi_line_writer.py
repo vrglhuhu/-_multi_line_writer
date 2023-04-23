2f84c17992d3245750f12e8851eee9d0ed4cf81b
vrglhuhu <cheanbvergel@gmail.com>
2023-04-23 11:23:00 +0800

    Adding footer, colors to the text, fixing the spacing and adding greetings.

--- _multi_line_writer.py
+++ _multi_line_writer.py
@@ -19,35 +19,57 @@ print("")
 # Ask for the name of the user
 name_user = input("\033[40mHow about you what is your name? \033[0m")
 print("")
-print("\033[40m\033[33mI welcome you on this program. \033[0m")
-print("\033[0m")
+print(f"\033[40m\033[33mHi, {name_user}! I welcome you on this program. \033[0m")
+print("")
 
 # DEFINE write_to_file
 def write_to_file():
  # OPEN file 'mylife.txt' in write mode
   with open('mylife.txt', 'w') as file:
   # SET line equal to user input of a line of text
-     line = input("Enter line: ")
+     line = input("\033[35mEnter line: \033[0m")
+     print("")
   # WRITE line to file with newline character
      file.write(line + '\n')
   # SET line_choice equal to user input of 'y' or 'n'
-     line_choice = input("Enter another line? (y/n): ")
+     line_choice = input("\033[34mDo you want to enter another line? (y/n): \033[0m")
+     if line_choice.lower() == "y":
+       # Make a greeting for the user 
+      print("") 
+      greetings = ["You are making great progress!.", "You have what it takes.", "You are amazing!", "It is awesome!"]
+      print(random.choice(greetings)) 
+     else:
+       print("You are making great progress!")
   # WHILE line_choice.lower is equal to 'y'
      while line_choice.lower() == 'y':
     # SET line equal to user input of a line of text
-      line = input("Enter line: ")
+      print("")
+      line = input("\033[35mEnter line: \033[0m")
     # WRITE line to file with newline character
       file.write(line + '\n')
     # SET line_choice equal to user input of 'y' or 'n'
-      line_choice = input("Enter another line? (y/n): ")
+      print("")
+      line_choice = input("\033[34mDo you want to enter another line? (y/n): \033[0m ")
+      print("")
     # Make a greeting for the user  
-      greetings = ["You are making great progress!", "You have what it takes", "You are amazing!, It is awesome!"]
+      greetings = ["You are making great progress!", "You have what it takes.", "You are amazing!", "It is awesome!"]
       print(random.choice(greetings))
 
   # CLOSE file
   file.close()
+  print("")
   print("\033[32mThe program is now writing it to txt file.\033[32m ")
   # State that the output is in the txt files
+  print("")
   print("\033[32mPlease have time to visit the mylife.txt, if you want to look on the outputs.\033[32m")
-  
+  print("")
+
+  # Create Footer
+  print(f"\033[40m\033[33mThank you for your time, {name_user}! \033[0m")
+  print("")
+  goodbye = pyfiglet.figlet_format("Visit me again", font = "digital" )
+  print (goodbye)
+  print("")
+
+# Call the method to run the code
 write_to_file()
